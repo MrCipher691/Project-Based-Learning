da96d29d3cdf5eb7474aef6bef8e17b6c489a926
MrCipher691 <ayush.ram691@gmail.com>
2021-04-24 01:25:30 +0530

    Assignment 5.1

---
+++ Shape.java
@@ -0,0 +1,31 @@
+abstract class Shape{
+    String color;
+    boolean filled;
+    
+    Shape(){
+        color="";
+        filled=true;
+    }
+    Shape(String color,boolean filled){
+        this.color=color;
+        this.filled=filled;              
+    }
+    String getcolor(){
+        return color;
+    }
+    void setcolor(String color){
+        this.color=color;
+    }
+    boolean isfilled(){
+        return filled;
+    }
+    void setfilled(boolean filled){
+        this.filled=filled;
+    }
+    abstract double getArea();
+    abstract double getPerimeter();
+    @Override
+    public String toString(){
+        return color;
+    }
+}
